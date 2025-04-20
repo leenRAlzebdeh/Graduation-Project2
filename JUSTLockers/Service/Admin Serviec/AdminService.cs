cb70b8e0e259fc4ec1df0f4e49ae80efd40aeb77
emasqwaider <emas_qwaider@yahoo.com>
2025-04-20 10:00:42 +0300

    order by

--- JUSTLockers/Service/Admin Serviec/AdminService.cs
+++ JUSTLockers/Service/Admin Serviec/AdminService.cs
@@ -1,5 +1,6 @@
 using JUSTLockers.Classes;
 using JUSTLockers.DataBase;
+using MailKit.Search;
 using Microsoft.AspNetCore.Connections;
 using Microsoft.Extensions.Configuration;
 using MySqlConnector;
@@ -465,7 +466,9 @@ public class AdminService : IAdminService
             if (!string.IsNullOrEmpty(searchCab) && searchCab != "")
             {
                 query += " AND cabinet_id like @searchCab";
-                command.Parameters.AddWithValue("@searchCab", $"%{searchCab}%");
+                command.Parameters.AddWithValue("@searchCab", "%" + searchCab.Trim() + "%");
+
+                // command.Parameters.AddWithValue("@searchCab", $"%{searchCab}%");
             }
 
 
@@ -499,7 +502,7 @@ public class AdminService : IAdminService
                 query += " AND wing = @wing";
                 command.Parameters.AddWithValue("@wing", wing);
             }
-
+            query += " order by department_name";
             command.CommandText = query;
 
             using (var reader = await command.ExecuteReaderAsync())
