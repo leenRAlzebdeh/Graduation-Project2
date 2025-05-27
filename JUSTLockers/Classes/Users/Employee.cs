d9005baa12353a20c7c553b7d97b85a0e1122f57
emasqwaider <emas_qwaider@yahoo.com>
2025-05-27 14:46:20 +0300

    nothing imp

--- JUSTLockers/Classes/Users/Employee.cs
+++ JUSTLockers/Classes/Users/Employee.cs
@@ -4,16 +4,16 @@ using System.Runtime.CompilerServices;
 
 public class Employee : User
 {
-    public string Location { get; set; }
+   // public string Location { get; set; }
    // public string DepartmentName { get; set; }
-    public Department SupervisedDepartment { get; set; }
+    //public Department SupervisedDepartment { get; set; }
     
    
 
     public Employee(int id, string name, string email, Department department,string location)
         : base(id, name, email)
     {
-        SupervisedDepartment = department;
-       this.Location = location;
+       // SupervisedDepartment = department;
+      // this.Location = location;
     }
 }
